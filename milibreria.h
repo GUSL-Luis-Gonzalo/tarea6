32cdf39b082677d2352d53225f5363ed1636f0a7
Luis Gonzalo Guzman Sanchez <105834318+GUSL-Luis-Gonzalo@users.noreply.github.com>
2022-05-22 16:44:43 -0500

    Add files via upload

---
+++ milibreria.h
@@ -0,0 +1,13 @@
+/* Autor: Luis Gonzalo Guzman Sanchez, Realizado: 07/04/2022
+Libreria para funciones comunes del programa C
+*/
+#ifndef _MILIBRERIA //Condicional para la librería
+#define _MILIBRERIA
+#include<stdio.h>
+#include<string.h>
+//Prototipos
+void leers(char *mensaje,char s[], int n);
+float leerf(char *mensaje, float inferior, float superior);
+int leerd(char *mensaje, int inferior, int superior);
+#include"milibreria.cpp"
+#endif
